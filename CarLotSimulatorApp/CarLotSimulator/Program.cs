7000d7105f24befc62491fdd00bb6bdc35bf0774
patrick-atienza25 <patienza25@gmail.com>
2022-10-27 13:59:04 -0700

    work in progress

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CarLotSimulator
 {
@@ -8,16 +9,38 @@ namespace CarLotSimulator
         {
             //TODO
 
-            //Create a seperate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
+            //Create a seperate class file called Car DONE
+            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable DONE
+            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise() DONE
+            //The methods should take one string parameter: the respective noise property DONE
 
 
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
-            //Call each of the methods for each car
+            //Call each of the methods for each car DONE
+            //Car car1 = new Car();
+            //car1.Make = "Tesla";
+            //car1.Model = "CyberTruck";
+            //car1.Year = 2021;
+            //car1.isDriveable = true;
+            //car1.EngineNoise("whoosh");
+            //car1.HonkNoise("ZAP");
 
+            //Car car2 = new Car();
+            //car2.Make = "Nissan";
+            //car2.Model = "Skyline";
+            //car2.Year = 1993;
+            //car2.isDriveable = false;
+            //car2.EngineNoise("VROOM VROOM");
+            //car2.HonkNoise("BEEEEEP");
+
+            //Car car3 = new Car();
+            //car3.Make = "Kia";
+            //car3.Model = "Sportage";
+            //car3.Year = 2023;
+            //car3.isDriveable = true;
+            //car3.EngineNoise("woom woom");
+            //car3.HonkNoise("meep meep");
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
@@ -30,4 +53,20 @@ namespace CarLotSimulator
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
         }
     }
+
+    //public class Car
+    //{
+    //    public int Year;
+    //    public string Make;
+    //    public string Model;
+    //    public bool isDriveable;
+    //    public string EngineNoise(string EngineNoise)
+    //    {
+    //        return EngineNoise;
+    //    }
+    //    public string HonkNoise(string HonkNoise)
+    //    {
+    //        return HonkNoise;
+    //    }
+    //}
 }
